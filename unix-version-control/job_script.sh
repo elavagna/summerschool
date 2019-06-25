30119a69a8840b3b1eac9ff48d164d6e11cd95b2
Enrico Lavagna <lavagna@fisica.unige.it>
2019-06-25 18:37:33 +0300

    new job script

--- unix-version-control/job_script.sh
+++ unix-version-control/job_script.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-#SBATCH -J my_job_name
+#SBATCH -J mistery_prog
 #SBATCH -o %J.out
 #SBATCH -e %J.err
 #SBATCH -n 24
 #SBATCH -p small
 #SBATCH -t 5
 #SBATCH --reservation=Summerschool
+
+aprun -n 24 ./prog
 
 
